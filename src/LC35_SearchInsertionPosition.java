1ebd974079ba0f6d7b8936f3afbd53479a7d5cc3
Songchuyue <2083681987@qq.com>
2026-09-01 17:10:01 +0800

    Implement binary search for LC35

--- src/LC35_SearchInsertionPosition.java
+++ src/LC35_SearchInsertionPosition.java
@@ -3,11 +3,26 @@ public class LC35_SearchInsertionPosition {
 		// 注意 找不到分为, 头插, 中插, 尾插
 		int len = nums.length;
 
-		for(int i = 0; i < len; i++) {
-			if(nums[i] >= target) return i;
+		int lef = 0, rig = len - 1, mid = (lef + rig) / 2;
+
+		while(lef <= rig) {
+			if(nums[mid] == target) return mid;
+			else if(nums[mid] < target) {// 插入位置只可能在mid右边
+				if(mid == len - 1 || nums[mid + 1] >= target) return mid + 1;// mid已经是最右边 或 mid右边的值不小于目标值
+
+				lef = mid + 1;
+			}
+			else if(nums[mid] > target) {// 插入位置在mid左边 或 mid本身
+				if(mid == 0 || nums[mid - 1] < target) return mid;// mid已经是最左边 或 mid左边的值小于目标值
+				else if(nums[mid - 1] == target) return mid - 1;
+
+				rig = mid - 1;
+			}
+
+			mid = (lef + rig) / 2;
 		}
 
-		return len;
+		return -1;
 	}
 
 	public static void main(String[] args) {
